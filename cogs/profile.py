c534fb845fb2f75152a41806fe52057917485907
Tominous <18189490+Tominous@users.noreply.github.com>
2020-02-16 15:38:16 +0000

    Update profile.py

--- cogs/profile.py
+++ cogs/profile.py
@@ -209,8 +209,8 @@ class Profile(commands.Cog):
         for key, value in keys.items():
             e.add_field(name=value, value=record[key] or 'N/A', inline=True)
 
-        # consoles = [f'__{v}__: {record[k]}' for k, v in keys.items() if record[k] is not None]
-        # e.add_field(name='Consoles', value='\n'.join(consoles) if consoles else 'None!', inline=False)
+         consoles = [f'__{v}__: {record[k]}' for k, v in keys.items() if record[k] is not None]
+         e.add_field(name='Consoles', value='\n'.join(consoles) if consoles else 'None!', inline=False)
         e.set_author(name=member.display_name, icon_url=member.avatar_url_as(format='png'))
 
         extra = record['extra'] or {}
